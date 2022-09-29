5de400c9abe30d080c2e2ec1c410e38c2e0154a9
Solo-Thunder <55307835+solothunder@users.noreply.github.com>
2022-09-29 10:17:43 +0900

    1.0

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from threading import Event
 import platform
 import os
-
+import sys
+import subprocess
 # 下準備 >>>>↓↓↓↓↓↓
 # バージョン
-elsversion = "0.6"
+elsversion = "1.0"
 # 遅延速度
 delay = 1
 delayt = 1.7
@@ -15,43 +16,59 @@ Event().wait(delay)
 # バージョン出力（ELS (Error Logs System) Python Version O.O）
 
 print("ELS (Error Logs System) Python Edition  Version", elsversion)
-
 Event().wait(delay)
 
 # OS判別
 osprat = platform.system()
-
-if osprat == "Windows" :
- print("Your Execution Operating System (OS) is" , osprat , "System")
-
+print("Your Execution Operating System (OS) is" , osprat , "System")
 
 Event().wait(delayt)
 
 print('ELS Start')
 Event().wait(delayt)
 
-# （ここまで）までマニュアルパス（手動）
+# Pingでインターネットつながってるか？
 print('-------------------- \nNetWork')
 Event().wait(delay)
-netkekka = "Manual Pass | \n--------------------"
-print(netkekka)
-#  (ここまで)
+host = "google.com"
+print("google.com")
+res = subprocess.run(["ping",host,"-n","2", "-w", "300"],stdout=subprocess.PIPE)
+# netkekka = "Manual Pass | \n--------------------"
+# print(netkekka)
+if res.returncode == 0 :
+    print("...")
+    Event().wait(delay)
+    print("OK |")
+else:
+    print("...")
+    Event().wait(delay)
+    print("Error (Stops the program after 15 seconds) |")
+    Event().wait("15")
+    sys.exit()
+print("-----------------------------")
+
+Event().wait(delay)
 
 # ファイル確認（pathの変数のところにファイル名（パスも）書いてね）
 print('-------------------- \nFile')
 path = ['addfiles/a.txt', 'addfiles/b.txt', 'addfiles/c.txt']
-for pathcount in path:
+for pathcount in path: 
+    is_file = os.path.isfile(pathcount)
+    if is_file:
+        print(f"{pathcount}")
+        Event().wait(delayaa)
+        print("...")
+        Event().wait(delayaa)
+        print("OK |")
+    else:
+        print(f"{pathcount}")
+        Event().wait(delayaa)
+        print("..........")
+        Event().wait(delayaa)
+        print("Error (Stops the program after 15 seconds) |")
+        Event().wait("15")
+        sys.exit()
 
- is_file = os.path.isfile(pathcount)
- 
- if is_file:
-    filekekka = (f"{pathcount} OK |")
-    print(f"{pathcount} OK |")
-    Event().wait(delayaa)
-else:
-    filekekka = (f"{pathcount} Caution |")
-    print(filekekka)
-    Event().wait(delayaa)
 
 print('--------------------')
 
@@ -61,11 +78,10 @@ def yes_no_input():
         if choice in ['y', 'ye', 'yes']:
             return True
         elif choice in ['n', 'no']:
-            return False
+            sys.exit()
 
 
 if __name__ == '__main__':
     if yes_no_input():
         print('OK Start')
         Event().wait(delay)
-          
